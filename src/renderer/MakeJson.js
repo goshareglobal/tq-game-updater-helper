92c41454c58c159821b4aa1d38daadeaea411b26
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2020-12-30 14:06:53 -0300

    cleaned up code

--- src/renderer/MakeJson.js
+++ src/renderer/MakeJson.js
@@ -2,10 +2,31 @@ module.exports = {
   MakeJson: () => {
     const fs = require("fs");
     const path = require("path");
-    const configFile = JSON.parse(
-      fs.readFileSync(process.env.PORTABLE_EXECUTABLE_DIR + "\\egu-config.json")
-    );
-    let localArray = [];
+
+    // Change window text
+    const showText = (text) => {
+      document.getElementById("LoaderContent").innerHTML = text;
+    };
+
+    // Output error
+    const alertError = (e) => {
+      showText("Error");
+      alert(e);
+    };
+
+    // Portable Executable Directory
+    const exeDir = process.env.PORTABLE_EXECUTABLE_DIR + "\\";
+
+    // Get config.json file
+    let configFile;
+    try {
+      configFile = JSON.parse(fs.readFileSync(exeDir + "egu-config.json"));
+    } catch (e) {
+      alertError(e);
+    }
+
+    // Define local files array
+    let localFiles = [];
 
     // Returns files list and takes the directory path as parameter
     const getFiles = async (dir, filesList = []) => {
@@ -21,56 +42,52 @@ module.exports = {
     };
 
     // Main function
-    getFiles(process.env.PORTABLE_EXECUTABLE_DIR + "\\").then((res) => {
-      res.forEach((file) => {
-        // Define file size
-        const size = fs.statSync(file).size;
+    try {
+      getFiles(exeDir).then((res) => {
+        res.forEach((file) => {
+          // Define file size
+          const size = fs.statSync(file).size;
 
-        // Define download url
-        const url =
-          configFile.url +
-          file
-            .replace(process.env.PORTABLE_EXECUTABLE_DIR + "\\", "")
-            .replace(/\\/g, "/");
+          // Define download url
+          const url =
+            configFile.url + file.replace(exeDir, "").replace(/\\/g, "/");
 
-        // Add all files
-        // If file should have a hash code, then it is considered special
-        if (
-          configFile.specialFiles.indexOf(
-            file
-              .replace(process.env.PORTABLE_EXECUTABLE_DIR + "\\", "")
-              .replace(/\\/g, "/")
-          ) > -1
-        ) {
-          const hash = require("crypto")
-            .createHash("sha1")
-            .update(fs.readFileSync(file))
-            .digest("base64");
-          file = localArray.push({ file, size, hash, url });
-        } else {
-          // else, file is not special
-          file = localArray.push({ file, size, url });
-        }
+          // Add all files
+          // If file should have a hash code, then it is considered special
+          if (
+            configFile.specialFiles.indexOf(
+              file.replace(exeDir, "").replace(/\\/g, "/")
+            ) > -1
+          ) {
+            const hash = require("crypto")
+              .createHash("sha1")
+              .update(fs.readFileSync(file))
+              .digest("base64");
+            file = localFiles.push({ file, size, hash, url });
+          } else {
+            // else file is not special
+            file = localFiles.push({ file, size, url });
+          }
 
-        // Remove files that should not be listed
-        localArray.forEach((e, i) => {
-          localArray = localArray.filter(
-            ({ file }) => !file.includes(configFile.unlistedFiles[i])
+          // Remove files that should not be listed
+          localFiles.forEach((e, i) => {
+            localFiles = localFiles.filter(
+              ({ file }) => !file.includes(configFile.unlistedFiles[i])
+            );
+            e.file = e.file.replace(exeDir, "");
+          });
+
+          // Write JSON
+          fs.writeFileSync(
+            exeDir + configFile.output,
+            JSON.stringify(localFiles)
           );
-          e.file = e.file.replace(
-            process.env.PORTABLE_EXECUTABLE_DIR + "\\",
-            ""
-          );
+
+          showText("JSON ready");
         });
-
-        // Write JSON
-        fs.writeFileSync(
-          process.env.PORTABLE_EXECUTABLE_DIR + "\\" + configFile.output,
-          JSON.stringify(localArray)
-        );
       });
-
-      document.getElementById("LoaderContent").innerHTML = "JSON ready!";
-    });
+    } catch (e) {
+      alertError(e);
+    }
   },
 };
